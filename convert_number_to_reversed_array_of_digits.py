de8b0358f1055e4b4c4da0afc069d636d8a14006
AlexeyZh <waltafunk@gmail.com>
2023-08-03 01:41:35 +0300

    Flake 8

--- convert_number_to_reversed_array_of_digits.py
+++ convert_number_to_reversed_array_of_digits.py
@@ -1,10 +1,9 @@
 '''
 Convert number to reversed array of digits
-Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+Given a random non-negative number, you have to return the digits of this \
+number within an array in reverse order.
 '''
 
 
 def digitize(n):
     return [int(i) for i in str(n)[::-1]]
-
-print(digitize(35231))
--- testing_1_2_3.py
+++ testing_1_2_3.py
@@ -1,9 +1,12 @@
 """
-Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+Your team is writing a fancy new text editor and you've been tasked with \
+    implementing the line numbering.
 
-Write a function which takes a list of strings and returns each line prepended by the correct number.
+Write a function which takes a list of strings and returns each line prepended\
+    by the correct number.
 
-The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+The numbering starts at 1. The format is n: string. Notice the colon and space\
+    in between.
 
 Examples: (Input --> Output)
 
