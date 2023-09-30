a57c2f79041fcf4fa0881c4d6b492e287dfafb78
Zhikharev_Alexey <81284552+ZhikharevAl@users.noreply.github.com>
2023-10-01 01:43:23 +0300

    Update income.py

--- income.py
+++ income.py
@@ -15,3 +15,18 @@ result = income(prices)
 
 def test_income():
     assert income([800, 1000, 1100, 1300]) == 400, "Should be 400"
+
+
+
+def test_zero_prices():
+    assert income([0, 0, 0, 0]) == 0, "Should be 0"
+
+
+def test_large_list():
+    prices = [i for i in range(1, 1001)]
+    assert income(prices) == sum(prices[1::2]) - sum(
+        prices[::2]), "Should be the difference between odd and even elements"
+
+
+def test_empty_list():
+    assert income([]) == 0, "Should be 0"
