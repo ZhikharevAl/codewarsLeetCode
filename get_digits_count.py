2feaae04a9d531aa0f1a0c7aacb62fda96802ae9
Zhikharev_Alexey <81284552+ZhikharevAl@users.noreply.github.com>
2023-10-09 18:09:01 +0300

    Update get_digits_count.py

--- get_digits_count.py
+++ get_digits_count.py
@@ -1,2 +1,14 @@
 def get_digits_count(message):
     return sum([int(i) for i in message if i.isdigit()])
+
+# import pytest
+
+# from main import get_digits_count
+
+
+# @pytest.mark.parametrize("message, result", [
+#     ("Lorem ipsum dolor sit 123 amet, consectetur adipiscing 456 elit.", 21),
+#     ("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 0),
+# ])
+# def test_get_digits_count(message, result):
+#     assert get_digits_count(message) == result
