c43ddb5a7ddd35d46ca5f8f89719ebc8757ff471
Zhikharev_Alexey <81284552+ZhikharevAl@users.noreply.github.com>
2023-10-16 19:51:57 +0300

    Create get_top_hashtags1.py

---
+++ get_top_hashtags1.py
@@ -0,0 +1,23 @@
+import re
+
+
+def get_top_hashtags(message, num_hashtags=5, min_word_length=5):
+    message = re.sub(r'\W', ' ', message).lower()
+    words = message.split()
+    hashtags = ['#{}'.format(word) for word in words if len(word) >= min_word_length][-num_hashtags:]
+    return " ".join(hashtags)
+
+# import pytest
+
+# from main import get_top_hashtags
+
+
+# @pytest.mark.parametrize('message, result', [
+#     ('Игра престолов - сериал, основанный на романах Джорджа Мартина, рассказывающий о борьбе за власть в вымышленном '
+#      'мире Вестероса', '#рассказывающий #борьбе #власть #вымышленном #вестероса'),
+#     ('Гладиатор - исторический эпос о римском генерале, который борется за свою свободу и мести', '#генерале #который '
+#                                                                                                   '#борется #свободу '
+#                                                                                                   '#мести'),
+# ])
+# def test_get_top_hashtags(message, result):
+#     assert get_top_hashtags(message) == result, 'Wrong result'
