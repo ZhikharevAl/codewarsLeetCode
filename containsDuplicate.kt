96da7751620b3490c1f6c32530323de570033f82
Zhikharev_Alexey <81284552+ZhikharevAl@users.noreply.github.com>
2023-10-20 15:07:58 +0300

    Create containsDuplicate.kt

---
+++ containsDuplicate.kt
@@ -0,0 +1,10 @@
+fun containsDuplicate(nums: IntArray): Boolean {
+    val seen = HashSet<Int>()
+    for (num in nums) {
+        if (seen.contains(num)) {
+            return true
+        }
+        seen.add(num)
+    }
+    return false
+}
