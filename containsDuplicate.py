16ab8da8d85b6fa6ebb8268c3a34cbc8a3065ff9
Zhikharev_Alexey <81284552+ZhikharevAl@users.noreply.github.com>
2023-10-20 14:38:35 +0300

    Update containsDuplicate.py

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,5 +1,14 @@
+
 def containsDuplicate(nums):
-    return len(set(nums)) != len(nums)
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
+
+# def containsDuplicate(nums):
+#     return len(set(nums)) != len(nums)
 
 
 # import pytest
