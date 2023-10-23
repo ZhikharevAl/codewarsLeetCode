bb52749063115582a46ab9e9d7edca26183aae6f
Zhikharev_Alexey <81284552+ZhikharevAl@users.noreply.github.com>
2023-10-23 17:39:07 +0300

    Update removeElements.py

--- removeElements.py
+++ removeElements.py
@@ -13,3 +13,14 @@ def removeElements(head, val):
 # ])
 # def test_isIsomorphic(head, val, result):
 #     assert removeElements(head, val) == result, 'Wrong result'
+
+    # def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+    #     dummy = ListNode(0)
+    #     dummy.next = head
+    #     prev = dummy
+    #     while prev.next:
+    #         if prev.next.val == val:
+    #             prev.next = prev.next.next
+    #         else:
+    #             prev = prev.next
+    #     return dummy.next
