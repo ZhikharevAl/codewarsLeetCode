c953de3cccc4ff25a5d962a1858cc9db6a377651
Zhikharev_Alexey <81284552+ZhikharevAl@users.noreply.github.com>
2024-01-25 15:02:29 +0300

    Create isUgly.py

---
+++ isUgly.py
@@ -0,0 +1,17 @@
+def isUgly(n):
+    """
+    :type n: int
+    :rtype: bool
+    """
+    if n <= 0:
+        return False
+    while n % 2 == 0:
+        n /= 2
+    while n % 3 == 0:
+        n /= 3
+    while n % 5 == 0:
+        n /= 5
+    return n == 1
+
+
+print(isUgly(6))
