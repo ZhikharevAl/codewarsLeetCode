98e1ab7ce40aa44cf82c5408f8dd633c1423efa1
Zhikharev_Alexey <81284552+ZhikharevAl@users.noreply.github.com>
2024-02-06 10:27:24 +0300

    Create canConstruct.py

---
+++ canConstruct.py
@@ -0,0 +1,14 @@
+def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        ransomNote = list(ransomNote)
+        magazine = list(magazine)
+        for i in ransomNote:
+            if i in magazine:
+                magazine.remove(i)
+            else:
+                return False
+        return True
